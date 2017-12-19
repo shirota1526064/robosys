5f1b17bbbcad8ac8b0cdeba41f11e2686f6f783c
shirota1526064 <s1526064KQ@s.chibakoudai.jp>
2017-12-20 02:07:43 +0900

    remove file

--- 006/.myled.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/program/robosys/006/myled.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/program/robosys/006/myled.ko /home/pi/program/robosys/006/myled.o /home/pi/program/robosys/006/myled.mod.o ;  true
--- 006/.tmp_versions/myled.mod
+++
@@ -1,3 +0,0 @@
-/home/pi/program/robosys/006/myled.ko
-/home/pi/program/robosys/006/myled.o
-
